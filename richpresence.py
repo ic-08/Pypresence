6b0f290e1946451f0b545b81805bddd4263d6294
ic-08 <89464943+ic-08@users.noreply.github.com>
2022-02-27 13:06:38 -0500

    Update richpresence.py

--- richpresence.py
+++ richpresence.py
@@ -1,14 +1,14 @@
 from pypresence import Presence
 import time
 
-client_id = '946962588183429190'  # Fake ID, put your real one here
-RPC = Presence(client_id,pipe=0)  # Initialize the client class
+client_id = '946962588183429190' 
+RPC = Presence(client_id,pipe=0)  
 RPC.connect() # Start the handshake loop
 
 
 
 print("Client connected. Game has started...")
-while True:  # The presence will stay on as long as the program is running
+while True:  
     day = time.time() - 1645845841
     day = day // 60 // 60 // 24
     RPC.update(state = f"Playing for {round(day)} day...", details="Studying Alone... (1 of 4)" , start = 1645845841, small_image = 'img.png', large_image = "crop_65465_", party_id= "ae488379-351d-4a4f-ad32-2b9b01c91657")  # Set the presence
